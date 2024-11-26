d787ce4dc5bda74a8af8292d43bae738674c31a9
Andrea Saccomanni <Andrea Saccomanni>
2024-11-26 22:15:47 +0100

    button per ogni genere

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,35 +1,66 @@
 import { Col, Container, Row } from "react-bootstrap";
-import books from "../horror.json";
+import horror from "../books/horror.json";
+import fantasy from "../books/fantasy.json";
+import history from "../books/history.json";
+import romance from "../books/romance.json";
+import scifi from "../books/scifi.json";
 
 import Card from "react-bootstrap/Card";
-const AllTheBooks = () => (
-  <Container>
-    <Row className="justify-content-center">
-      {books.map((book) => {
-        return (
-          <Col key={book.asin} xs={12} sm={6} md={4} lg={4} xl={4} style={{ marginBottom: "20px" }}>
-            <Card>
-              <Card.Img variant="top" src={book.img} />
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <div>
-                  <Card.Text className="d-flex justify-content-around mt-4">
-                    <p>
-                      <strong>Category: </strong>
-                      {book.category}
-                    </p>
-                    <p>
-                      <strong>Price: </strong>
-                      {book.price}$
-                    </p>
-                  </Card.Text>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        );
-      })}
-    </Row>
-  </Container>
-);
+import { Component } from "react";
+import { Button } from "react-bootstrap";
+class AllTheBooks extends Component {
+  state = {
+    books: fantasy
+  };
+  render() {
+    return (
+      <Container>
+        <div className="d-flex gap-3 justify-content-center my-3">
+          <Button onClick={() => this.setState({ books: history })} variant="success">
+            History
+          </Button>
+          <Button onClick={() => this.setState({ books: horror })} variant="warning">
+            Horror
+          </Button>
+          <Button onClick={() => this.setState({ books: fantasy })} variant="dark">
+            Fantasy
+          </Button>
+          <Button onClick={() => this.setState({ books: romance })} variant="info">
+            Romance
+          </Button>
+          <Button onClick={() => this.setState({ books: scifi })} variant="danger">
+            Scifi
+          </Button>
+        </div>
+
+        <Row xs={1} sm={2} md={3} lg={4} className="gy-3">
+          {this.state.books.map((book) => {
+            return (
+              <Col key={book.asin}>
+                <Card>
+                  <Card.Img variant="top" src={book.img} />
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <div>
+                      <Card.Text className="d-flex justify-content-around mt-4">
+                        <p>
+                          <strong>Category: </strong>
+                          {book.category}
+                        </p>
+                        <p>
+                          <strong>Price: </strong>
+                          {book.price}$
+                        </p>
+                      </Card.Text>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    );
+  }
+}
 export default AllTheBooks;
